1bdb49afb7edadaa035fbe1ab9010899218941cf
Tomokki <134517597+Tomokki@users.noreply.github.com>
2023-06-18 22:42:33 +0900

    Add files via upload

---
+++ kadai_013/square.js
@@ -0,0 +1,8 @@
+function calculate(value) {
+  const result = value * value;
+  return result;
+}
+
+const inputValue = 10; // 引数の値を指定します
+const squareResult = calculate(inputValue); // 関数を呼び出して計算結果を取得します
+console.log(squareResult); // 計算結果をコンソールに出力します
